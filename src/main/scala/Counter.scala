75e44c9224013c29ad8d209578d357871054ab04
Eric Biernacki <54508395+ebiernacki@users.noreply.github.com>
2024-01-11 14:49:34 -0600

    Create Counter.scala

---
+++ src/main/scala/Counter.scala
@@ -0,0 +1,22 @@
+/*
+ * 
+ * A simple counter example with configurable bit width and with a test bench.
+ * Code from: https://github.com/schoeberl/chisel-examples/blob/master/src/main/scala/simple/Counter.scala
+ */
+
+import chisel3._
+
+/**
+ * A simple, configurable counter that wraps around.
+ */
+class Counter(size: Int) extends Module {
+  val io = IO(new Bundle {
+    val out = Output(UInt(size.W))
+  })
+
+  val r1 = RegInit(0.U(size.W))
+  r1 := r1 + 1.U
+
+  io.out := r1
+}
+
